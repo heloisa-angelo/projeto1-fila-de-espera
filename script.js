c3edfe36431dbd28da44390b45e9d8f6f25b0277
Ana Heloisa <heloisaangelo.17@gmail.com>
2024-10-17 10:01:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const queue = [`1 - Maria
+                2- João
+                3- Ana`]
+
+let menu1 =prompt('Novo paciente:')
+let menu2 = queue.shift(prompt('Consultar paciene:'))
+
+for( let i = 0; i > queue.lenght; i++ ) {
+ const pacients = queue[i]
+}
+if () {
+
+}
